5dff51d2df1f10312551150530ad5d2f37c27c19
DESKTOP-R889J86\yangrui <youngray9706@gmail.com>
2026-05-12 21:47:52 +0800

    部署上去后报错处理

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
+/*
+ * @Author: YangRui
+ * @Date: 2026-05-11 21:35:34
+ * @LastEditors: YangRui
+ * @LastEditTime: 2026-05-12 21:47:27
+ * @Description: 请输入
+ */
+const {
+  defineConfig
+} = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: "./" // 只需要加这一行！
 })
